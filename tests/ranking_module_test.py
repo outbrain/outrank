dcf4a704666954bed02546ae77941203eeb3402c
bskrlj <bskrlj@outbrain.com>
2024-01-05 11:36:18 +0100

    test

--- tests/ranking_module_test.py
+++ tests/ranking_module_test.py
@@ -33,6 +33,7 @@ class args:
     target_ranking_only: str = 'True'
     interaction_order: int = 3
     combination_number_upper_bound: int = 1024
+    verbosity_level: int = 1
 
 
 class CompareStrategiesTest(unittest.TestCase):
