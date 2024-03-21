344be92c2227877f382da2b57d21b2831d828def
bmramor <bmramor@outbrain.com>
2024-03-21 21:39:04 +0000

    formatting

--- outrank/task_selftest.py
+++ outrank/task_selftest.py
@@ -1,16 +1,21 @@
 # helper set of methods that enable anywhere verification of core functions
 from __future__ import annotations
+
 import logging
 import os
 import shutil
 import subprocess
+
 import pandas as pd
+
 logging.basicConfig(
     format='%(asctime)s - %(message)s',
     datefmt='%d-%b-%y %H:%M:%S',
 )
 logger = logging.getLogger('syn-logger')
 logger.setLevel(logging.DEBUG)
+
+
 def conduct_self_test():
     # Simulate full flow, ranking only
     subprocess.run(
@@ -22,10 +27,12 @@ def conduct_self_test():
     )
 
     dfx = pd.read_csv('ranking_outputs/pairwise_ranks.tsv', sep='\t')
+
     logger.info("Verifying output's properties ..")
     assert dfx.shape[0] == 120
     assert dfx.shape[1] == 3
     assert dfx['FeatureA'].values.tolist().pop() == 'label-(81; 100)' or dfx['FeatureB'].values.tolist().pop() == 'label-(81; 100)'
+
     to_remove = ['ranking_outputs', 'test_data_synthetic']
     for path in to_remove:
         if os.path.exists(path) and os.path.isdir(path):
