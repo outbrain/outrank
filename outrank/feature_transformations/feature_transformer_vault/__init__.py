9a4f2acb3bfcb1cdfa992139134ec8027c464efc
bskrlj <bskrlj@outbrain.com>
2024-07-18 11:38:11 +0200

    Extended

--- outrank/feature_transformations/feature_transformer_vault/__init__.py
+++ outrank/feature_transformations/feature_transformer_vault/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from outrank.feature_transformations.feature_transformer_vault.default_transformers import DEFAULT_TRANSFORMERS
+from outrank.feature_transformations.feature_transformer_vault.default_transformers import EXTENDED_TRANSFORMERS
 from outrank.feature_transformations.feature_transformer_vault.default_transformers import MINIMAL_TRANSFORMERS
 from outrank.feature_transformations.feature_transformer_vault.fw_transformers import \
     FW_TRANSFORMERS
@@ -9,4 +10,5 @@ _tr_global_namespace = {
     'default': DEFAULT_TRANSFORMERS,
     'minimal': MINIMAL_TRANSFORMERS,
     'fw-transformers': FW_TRANSFORMERS,
+    'extended': EXTENDED_TRANSFORMERS,
 }
