695e7f90af0934167127ef103ba779e24eb8f07d
bskrlj <bskrlj@outbrain.com>
2024-08-16 20:57:01 +0200

    more tests

--- tests/cov_heu_test.py
+++ tests/cov_heu_test.py
@@ -42,3 +42,15 @@ class TestMaxPairCoverage(unittest.TestCase):
         array2 = np.array([2, 2, 2, 2, 2])
         result = max_pair_coverage(array1, array2)
         self.assertEqual(result, 1.0)
+
+    def test_high_collision_potential(self):
+        array1 = np.array([1] * 1000)
+        array2 = np.array([2] * 1000)
+        result = max_pair_coverage(array1, array2)
+        self.assertEqual(result, 1.0)
+
+    def test_very_large_arrays(self):
+        array1 = np.random.randint(0, 1000, size=1000000)
+        array2 = np.random.randint(0, 1000, size=1000000)
+        result = max_pair_coverage(array1, array2)
+        self.assertTrue(0 <= result <= 1)
